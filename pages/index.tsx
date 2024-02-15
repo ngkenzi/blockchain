2806fa1a50969ba8155f2aed9f9f2bd2341fc1b0
ngkenzi123 <kenzi@ciphertech.io>
2024-02-15 13:08:54 +0800

    mobile navbar hamburger menu added

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,13 @@ import { useRouter } from "next/router";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Navbar from "../components/Navbar";
 import styles from "./index.module.css";
+import HamburgerMenu from "../components/HamburgerMenu";
 
 const Home = () => {
   const [hasSubmitted, setHasSubmitted] = useState(false);
   const [isSubmissionStatusLoaded, setIsSubmissionStatusLoaded] =
     useState(false);
-
+  const [windowWidth, setWindowWidth] = useState(undefined);
   const router = useRouter();
 
   const checkSubmission = async () => {
@@ -32,13 +33,18 @@ const Home = () => {
 
   useEffect(() => {
     checkSubmission();
+    setWindowWidth(window.innerWidth);
   }, []);
 
   return (
     <>
       <div className={styles.homepage_container}>
         <section>
-          <Navbar />
+          {windowWidth <= 600 && windowWidth >= 375 ? (
+            <HamburgerMenu />
+          ) : (
+            <Navbar />
+          )}
           <div className={styles.intro}>
             <div className={styles.upper_content}>
               <p>WELCOME TO BEINGU</p>
@@ -98,146 +104,6 @@ const Home = () => {
                   <p>
                     Find out how you can take control and shape your digital
                     future with confidence.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div
-        style={{
-          minHeight: "70vh",
-          backgroundImage: 'url("/assets/what_we_do_bg.png")',
-          backgroundSize: "100% 100%",
-          backgroundRepeat: "no-repeat",
-          backgroundPosition: "center",
-          position: "relative",
-        }}
-      >
-        <div
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            backgroundColor: "rgba(0, 0, 0, 0.7)",
-            display: "flex",
-            justifyContent: "center",
-            flexDirection: "column",
-          }}
-        >
-          <div
-            style={{
-              color: "white",
-              fontWeight: "900",
-              fontSize: "43px",
-              letterSpacing: "2px",
-              opacity: "80%",
-              display: "flex",
-              justifyContent: "center",
-              flexDirection: "column",
-              padding: "0 150px",
-            }}
-          >
-            <div
-              style={{
-                alignSelf: "center",
-                padding: "80px 0 40px 0",
-              }}
-            >
-              <span style={{ color: "#9A30DB" }}>OUR</span>
-              <span> CUSTOMERS</span>
-            </div>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "space-between",
-                alignItems: "center",
-              }}
-            >
-              {/* Motive for Users */}
-              <div
-                style={{
-                  width: "33%",
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                }}
-              >
-                <img
-                  src="/assets/medal.png"
-                  alt="Description of Image"
-                  style={{
-                    maxWidth: "50%",
-                    maxHeight: "50%",
-                    transform: "scale(0.5)",
-                  }}
-                />
-                <div
-                  style={{
-                    fontSize: "15px",
-                    fontWeight: "400",
-                    padding: "10px",
-                    textAlign: "center",
-                    letterSpacing: "1px",
-                    opacity: "73%",
-                    fontFamily: "Kanit",
-                  }}
-                >
-                  <p>
-                    <strong>Users</strong>
-                  </p>
-                  <p>
-                    Empower yourself with verifiable{" "}
-                    <strong>digital twins</strong> of your certifications,
-                    corporate rankings, and skillsets. Whether you're a
-                    professional, student, or individual looking to showcase
-                    your abilities, learn how BeingU can help you stand out in a
-                    competitive <strong>Web3 landscape</strong>.
-                  </p>
-                </div>
-              </div>
-              {/* Motive for Organisations */}
-              <div
-                style={{
-                  width: "33%",
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                }}
-              >
-                <img
-                  src="/assets/medal.png"
-                  alt="Description of Image"
-                  style={{
-                    maxWidth: "50%",
-                    maxHeight: "50%",
-                    transform: "scale(0.5)",
-                  }}
-                />
-                <div
-                  style={{
-                    fontSize: "15px",
-                    fontWeight: "400",
-                    padding: "10px",
-                    textAlign: "center",
-                    letterSpacing: "1px",
-                    opacity: "73%",
-                    fontFamily: "Kanit",
-                  }}
-                >
-                  <p>
-                    <strong>Organisations</strong>
-                  </p>
-                  <p>
-                    Streamline your corporate structure, training processes and
-                    enhance in-house credibility with BeingU for organisations.
-                    Discover how our platform empowers seamless integration,
-                    ease of verification and authentication of prospective
-                    employees in our secured ecosystem.
                   </p>
                 </div>
               </div>
