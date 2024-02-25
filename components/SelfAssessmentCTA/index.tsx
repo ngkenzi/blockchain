7d185e3a1a16d12d8755acd8b6b11eedc604b3e3
ngkenzi123 <kenzi@ciphertech.io>
2024-02-25 22:12:03 +0800

    fix bug

--- components/SelfAssessmentCTA/index.tsx
+++ components/SelfAssessmentCTA/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useRouter } from "next/router";
 import styles from "./style.module.css";
 
@@ -6,27 +6,22 @@ const SelfAssessmentCTA = () => {
   const router = useRouter();
   const [visible, setVisible] = useState(true);
 
-  useEffect(() => {
-    if (!visible) {
-      const timeoutId = setTimeout(() => setVisible(false), 500);
-      return () => clearTimeout(timeoutId); // Cleanup function for unmounted component
-    }
-  }, [visible]);
-
   const handleCTAClick = () => router.push("/assessment");
 
   const handleClose = () => setVisible(false);
 
   return (
-    <div className={`${styles.banner} ${!visible && styles.slideDown}`}>
-      <span>
-        Level up job opportunities with your 1st self-assessment cert today
-      </span>
-      <div className={styles.button_container}>
-        <button onClick={handleCTAClick}>Start Self-Assessment</button>
-        <button onClick={handleClose}>Close</button>
+    visible && (
+      <div className={styles.banner}>
+        <span>
+          Level up job opportunities with your 1st self-assessment cert today
+        </span>
+        <div className={styles.button_container}>
+          <button onClick={handleCTAClick}>Start Self-Assessment</button>
+          <button onClick={handleClose}>Close</button>
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
